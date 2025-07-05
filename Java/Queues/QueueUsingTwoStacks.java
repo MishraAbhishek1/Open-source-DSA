89208be948508cc54046b509b813297efb510bd6
CaptainADx <captainabhijeet567@gmail.com>
2025-07-05 14:11:32 +0530

    Queue using Two stacks code is added

---
+++ Java/Queues/QueueUsingTwoStacks.java
@@ -0,0 +1,48 @@
+import java.util.Stack;
+
+class QueueUsingTwoStacks{
+    static class queueUsingTwoStacks{
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+
+        void add(int data){
+            if(s1.isEmpty()){
+                s1.push(data);
+                return;
+            }
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            s1.push(data);
+            
+            while(!s2.isEmpty()){
+                s1.push(s2.pop());
+            }
+        }
+
+        int remove(){
+            if(s1.isEmpty()){
+                System.out.println("Stack is Empty");
+                return -1;
+            }
+            return s1.pop();
+        }
+
+        void printQueue(){
+            while(!s1.isEmpty()){
+                System.out.println(s1.pop());
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        queueUsingTwoStacks q = new queueUsingTwoStacks();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.remove();
+        q.add(5);
+        q.printQueue();
+    }
+}
